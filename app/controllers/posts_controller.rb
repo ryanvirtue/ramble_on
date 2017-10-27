8cf80112ff0092409efcfcc7cfc7c9b65f58091d
Ryan Virtue <ryan@hashbang.co.nz>
2017-10-27 18:55:44 +1300

    Create models and controllers for creating posts 🏔
    * Create Tag, Post models
    * Create Tags, Posts, Blog, Dashboard controllers
    * Change to tests to RSpec

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,21 @@
+class PostsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    # TODO
+  end
+
+  def destroy
+    # TODO
+  end
+
+  def edit
+  end
+
+  def update
+    # TODO
+  end
+  
+end
