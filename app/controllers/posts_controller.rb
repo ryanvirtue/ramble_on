ce998ad3557c187b58dfa230bbd28b8b7a848c16
Ryan Virtue <ryan@hashbang.co.nz>
2017-11-02 22:00:43 +1300

    create and edit posts as a user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,42 @@
 class PostsController < ApplicationController
 
-  before_action :authenticate_user!
-  
+  before_action :authenticate_user!, except: :show
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def new
     @post = Post.new
   end
 
   def create
-    post = Post.new(post_params)
-    if post.save!
+    @post = Post.new(post_params)
+    if @post.save
       redirect_to dashboard_path
+    else
+      render :new
     end
   end
 
   def destroy
-    # TODO
+    @post = Post.find(params[:id])
+    if @post.delete
+      redirect_to dashboard_path
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
-    # TODO
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
