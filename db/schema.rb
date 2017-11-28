0cbe22b6f90858c4bdff0e32ea6d963417c49e4b
Ryan Virtue <ryan@hashbang.co.nz>
2017-11-28 21:50:13 +1300

    Add friendly id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102090234) do
+ActiveRecord::Schema.define(version: 20171128084402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,18 @@ ActiveRecord::Schema.define(version: 20171102090234) do
     t.index ["type"], name: "index_ckeditor_assets_on_type"
   end
 
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "subtitle"
@@ -36,6 +48,7 @@ ActiveRecord::Schema.define(version: 20171102090234) do
     t.datetime "updated_at", null: false
     t.boolean "publish"
     t.datetime "published_at"
+    t.string "slug"
   end
 
   create_table "tags", force: :cascade do |t|
